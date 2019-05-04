516b743f4fec621c01a7bdca6bb8e72f6a6ab144
AlejandroTE <alejandro.taesp@gmail.com>
2019-05-04 20:07:16 +0100

    First commit

---
+++ dm2e/app/src/main/java/com/example/dm2e/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.dm2e;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.dm2e.view.CreateAccountActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void goCreateAccount(View view) {
+
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ dm2e/app/src/main/java/com/example/dm2e/view/CreateAccountActivity.java
@@ -0,0 +1,30 @@
+package com.example.dm2e.view;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.example.dm2e.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.toolbar_title_createaccount),true);
+    }
+
+
+    public void showToolbar(String title, boolean upButton) {
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+
+    }
+}
