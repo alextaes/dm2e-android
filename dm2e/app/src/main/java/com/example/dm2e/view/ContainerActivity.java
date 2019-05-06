cd2be40da51b73485e42e92028ed38d41893303a
AlejandroTE <alejandro.taesp@gmail.com>
2019-05-06 21:23:09 +0100

    RecyclerView

--- dm2e/app/src/main/java/com/example/dm2e/view/ContainerActivity.java
+++ dm2e/app/src/main/java/com/example/dm2e/view/ContainerActivity.java
@@ -1,9 +1,16 @@
 package com.example.dm2e.view;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.dm2e.R;
+import com.example.dm2e.view.fragment.HomeFragment;
+import com.example.dm2e.view.fragment.ProfileFragment;
+import com.example.dm2e.view.fragment.SearchFragment;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabReselectListener;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class ContainerActivity extends AppCompatActivity {
 
@@ -11,5 +18,45 @@ public class ContainerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setDefaultTab(R.id.home);
+
+        //Listener para cambiar fragment dentro del container segun la opcion elegida
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int tabId) {
+                switch (tabId) {
+                    case R.id.home:
+                        HomeFragment homeFragment = new HomeFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.container, homeFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+
+                        break;
+                    case R.id.profile:
+                        ProfileFragment profileFragment = new ProfileFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.container, profileFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        break;
+                    case R.id.search:
+                        SearchFragment searchFragment = new SearchFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.container, searchFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        break;
+                }
+            }
+        });
     }
 }
